6b4015d52f053cd0d486c1a6d7e9c676e2efd1cc
Arvind <karvi90@yahoo.com>
2017-04-03 19:12:49 -0700

    More dialogue

--- Assignment 1/dialog.py
+++ Assignment 1/dialog.py
@@ -2,7 +2,7 @@
 # This program runs a dialog between two agents, which must be defined
 # elsewhere as separate modules.
 
-import Shrink3 as agentA
+import Ieve as agentA
 import Hearnone as agentB
 
 N_TURNS = 5
@@ -10,7 +10,7 @@ N_TURNS = 5
 turn = 0
 print(str(turn)+"A: "+agentA.agentName() + ': ' + agentA.introduce()+"\n")
 print(str(turn)+"B: "+agentB.agentName() + ': ' + agentB.introduce()+"\n")
-remark = "Good morning."
+remark = "the binary"
 for i in range(N_TURNS):
     turn += 1
     remark = agentA.respond(remark)
