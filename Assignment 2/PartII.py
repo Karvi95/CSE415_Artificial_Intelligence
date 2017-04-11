dba5fb53d46478969aa7cad65531959754c72e03
Arvind <karvi90@yahoo.com>
2017-04-11 15:18:02 -0700

    uhmmm.

--- Assignment 2/PartII.py
+++ Assignment 2/PartII.py
@@ -106,32 +106,47 @@ def process(info):
 #                    print("ISA: ", ISA)
  #                   print("INCLUDES: ", INCLUDES)
   #                  print("FIND CHAIN: ", find_chain(items[1], items[3]))
-                    
 
+                                        
+                    check = directRedundancyCheck(items[1], items[3])
+                    if check == "Printed.":
+                        return
                     #if items[1]'s list has a string that item[3]'s list has, then indirect redundant
+                    #print('ISA: ' + str(ISA))
+                    # for attr in list(set(ISA[items[1]]) & set(ISA.keys())):
+                    #     currentValues = ISA[attr]
+                    #     while(items[3] not in currentValues):
+                    #         for value in ISA[attr]:
+                    #             try:
+                    #                 currentValues = ISA[value]
+                    #             except KeyError:
+                    #                 return
 
-                    indirectRedundancyCheck(items[1], items[3])
+                    #     print("You don't have to tell me that.")
+                    #     print('')
+                    #     ISA[items[1]].remove(items[3])
+                    #     return
+                    #print('HI')
+                    check = indirectRedundancyCheck(items[1], items[3])
+                    if check == "Printed.":
+                        return
+                    #print('Bye')
 
                     
                     #Make private function; should I do this recursively or check Union-Find (GO BACKWARDS FROM EATING TO FISH BASE CASE IS KEY ERROR OR IT EQUALS so use .keys()!!)?
 					
-                    for attr in list(set(ISA[items[1]]) & set(ISA.keys())):
-                        currentValues = ISA[attr]
-                        while(items[3] not in currentValues):
-                        	for value in ISA[attr]:
-                        		currentValues = ISA[value]
-
-                        print("You don't have to tell me that.")
-                        ISA[items[1]].remove(items[3])
-                        return
+                    
 
                     print("I understand.")
+                    print('')
                     return
                 else:
                     print("You don't have to tell me that.")
+                    print('')
                     return
             else:
                 print ("You told me that earlier.")
+                print('')
                 return
 
     result_match_object = query_pattern.match(info)
@@ -217,30 +232,190 @@ def find_chain(x, z):
                 temp.insert(0, [x,y])
                 return temp
 
+def directRedundancyCheck(first, second):
+    toReturn = "Didn't Print"
+    for attr in list(set(ISA[first]) & set(ISA.keys())):
+        currentValues = ISA[attr]
+        while(second not in currentValues):
+            for value in ISA[attr]:
+                try:
+                    currentValues = ISA[value]
+                except KeyError:
+                    return toReturn
+
+        print("You don't have to tell me that.")
+        print('')
+        toReturn = "Printed."
+        ISA[first].remove(second)
+        return toReturn
+
 def indirectRedundancyCheck(first, second):
-	#print("ISA: ", ISA)
-	#print("INCLUDES: ", INCLUDES)
-	if first in INCLUDES.keys():
-		#print("FIRST: ", first)
-		#print("FIRST's LIST: ", INCLUDES[first]) 
-		#print("SECOND: ", second)
-		for included in INCLUDES[first]:
-			if included in INCLUDES[second]:
-				chain = find_chain(included, second)
-#				print('FIND CHAIN: ', chain)
-				if len(chain) == 1:
-					print('Your earlier statement that ' + report_link(chain[0]) + ' is redundant.')
-				elif len(chain) >= 1:
-					print('The following statements you made earlier are now all redundant:')
-					for link in chain:
-						print(report_link, ';')
-		return					
+    output = list(indirectRedundancyCheckHelper(first, second))
+    listComparisonList = []
+    if output:
+        print("OUTPUT: " + str(output))
+        print("")
+        DESCENDENTS = [item for sublist in output for item in sublist]
+        # print("DESCENDENTS: " + str(DESCENDENTS))
+        # for descendent in DESCENDENTS:
+        #     print("THIS DESCENDENT: " + descendent)
+        #     unvisitedUniverseNew = [x for x in (INCLUDES[descendent])]
+        #     print("NEW UNIVERSE: " + str(unvisitedUniverseNew))
+        #     for relationNew in unvisitedUniverseNew:
+        #         print("NEW RELATION: " + str(relationNew))
+        #         comparison.add(relationNew)
+        #         print(comparison)
+        #         secondDegreesNew = INCLUDES[relationNew]
+        #         print("NEW SECOND DEGREES: " + str(secondDegreesNew))
+        #         for childNew in secondDegreesNew:
+        #             comparison.add(childNew)
+        #             try: 
+        #                 secondDegreesNew = INCLUDES[childNew]        
+        #             except KeyError:
+        #                 print("COMPARISON: " + str(pathNew))
+        #                 yield path
+
+
+
+
+def indirectRedundancyCheckHelper(first, second, path=None):
+    print("ISA: " + str(ISA))
+    print("INCLUDES: " + str(INCLUDES))
+    print("")
+
+
+    if path is None:
+        path = [first]
+
+    if first == second:
+        try:
+            yield path
+        except KeyError:
+            pass
+
+    childrenOfFirst = dfs(INCLUDES, first)
+    childrenOfFirst.remove(first)
+    
+    parentsDict = {}
+
+    for child in childrenOfFirst:
+        parents = dfs(ISA, child)
+        parents.remove(child)
+        parentsDict[child] = parents
+
+    print("PARENTS DICTIONARY: " + str(parentsDict))
+
+    if second in ISA.keys():
+        secondsParents = ISA[second]
+        for key in parentsDict.keys():
+            values = parentsDict[key]
+            print(values)
+            inCommon = list(set(secondsParents).intersection(values))
+            for common in inCommon:
+                chain = find_chain(child, common)
+                print('chain ' + str(chain))
+                if len(chain) == 1:
+                    get_isa_list(chain[0][0]).remove(common)
+                    get_includes_list(common).remove(child)
+                    print('Your earlier statement that ' + report_link(chain[0]) + ' is redundant.')
+                    print("ISA: " + str(ISA))
+                    print("INCLUDES: " + str(INCLUDES))
+                # elif len(chain) >= 1:
+                #     print('The following statements you made earlier are now all redundant:')
+                #     for link in chain:
+                #         print(report_link + ';')
+
+def isAncestor():
+    print("HI")
+
+def dfs(graph, child, visited = None):
+    if visited == None:
+        visited = []
+    if child in visited:
+        return
+
+    visited.append(child)
+
+    try: 
+        for each in [x for x in graph[child] if x not in visited]:
+            dfs(graph, each, visited)
+    except KeyError:
+        pass
+
+    return visited
+
+    # for finalizedChild in childrenOfFirst:
+    #     parents = ISA[finalizedChild]
+    #     print("PARENTS: " + str(parents))
+    #     for parent in parents:
+    #         parentsOfChildren.append(parent)
+    #         if parent in ISA.keys():
+    #             while parent in ISA.keys():
+    #                 parents = ISA[parent]
+    #                 break
+    # print("PARENTS OF CHILDREN: " + str(parentsOfChildren))
+
+
+
+    # unvisitedUniverse = [x for x in list(set(INCLUDES[first]) & set(INCLUDES.keys())) if x not in path]
+    # #print("UNVISITED UNIVERSE: " + str(unvisitedUniverse))
+    # for relation in unvisitedUniverse:
+    #     #print(relation)
+    #     path.append(relation)
+    #     secondDegrees = INCLUDES[relation]
+    #     #print("SECOND DEGREES: " + str(secondDegrees))
+    #     for child in secondDegrees:
+    #         path.append(child)
+    #         try: 
+    #             secondDegrees = INCLUDES[child]        
+    #         except KeyError:
+    #             #print("PATH: " + str(path))
+    #             path.remove(first)
+    #             yield path
+    #     # for eachPath in indirectRedundancyCheckHelper(relation, second, path.append(relation)):
+        #     yield eachPath
+            
+    #print('IN')
+    #stack = [(start, [start])]
+    #print(stack)
+    # while stack:
+    #     (vertex, path) = stack.pop()
+    #     for next in graph[vertex] - set(path):
+    #         if next == goal:
+    #             yield path + [next]
+    #         else:
+    #             stack.append((next, path + [next]))
+    # return
+
+
+    # print("ISA: " + str(ISA))
+    # print("INCLUDES: " + str(INCLUDES))
+    # if first in INCLUDES.keys():
+    #     print("FIRST: " + first)
+    #     print("FIRST's LIST: " + str(INCLUDES[first])) 
+    #     print("SECOND: " + second)
+    #     for included in INCLUDES[first]:
+    #         print("SECOND's LIST: " + str(INCLUDES[second]))
+    #         if included in INCLUDES[second]:
+    #             chain = find_chain(included, second)
+    #             print('INCLUDED ' + included)
+    #             if len(chain) == 1:
+    #                 get_isa_list(chain[0][0]).remove(second)
+    #                 get_includes_list(second).remove(included)
+    #                 print('Your earlier statement that ' + report_link(chain[0]) + ' is redundant.')
+    #                 print("ISA: " + str(ISA))
+    #                 print("INCLUDES: " + str(INCLUDES))
+    #             elif len(chain) >= 1:
+    #                 print('The following statements you made earlier are now all redundant:')
+    #                 for link in chain:
+    #                     print(report_link + ';')
+    #     return		
 
 
 
 def test() :
-	indirectTest1()
-	#indirectTest2()
+	#indirectTest1()
+	indirectTest2()
     #directTest()
 
 def directTest():
